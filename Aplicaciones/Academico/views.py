24077236a727e392cd968a73e109ed7c666ca0d0
elianisdev <elianismedina05@outlook.com>
2023-09-02 20:09:51 -0500

    finished form

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Curso
+
+
 # Create your views here.
 def home(request):
     cursos = Curso.objects.all()
-    return render(request,"gestionCursos.html",{"cursos": cursos})
+    return render(request, "gestionCursos.html", {"cursos": cursos})
 
+
+def registrarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['numCreditos']
+
+    curso = Curso.objects.create(
+        codigo=codigo, nombre=nombre, creditos=creditos
+    )
+    return redirect('/')
